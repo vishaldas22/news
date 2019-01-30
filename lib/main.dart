fb3525695378b40a4d995467f2a9f234cb78f67c
Bishal das <31515160+vishaldas22@users.noreply.github.com>
2019-01-30 10:19:49 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:news/detailScreen.dart';
+import 'package:news/login.dart';
+import 'package:news/profile/selectProPic.dart';
+import 'package:news/signup.dart';
 import 'package:news/ui/bookmark.dart';
 import 'package:news/ui/category.dart';
 import 'package:news/ui/homepage.dart';
@@ -11,16 +15,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
           routes: <String ,WidgetBuilder>{
             '/category': (BuildContext context) => Category(),
             '/search': (BuildContext context) => Search(),
             '/bookmark': (BuildContext context) => Bookmark(),
             '/detailscreen': (BuildContext context) => DetailScreen(),
+            '/homepage': (BuildContext context) => HomePage(),
+            '/signup': (BuildContext context) => SignUp(),
+            '/selectpic': (BuildContext context) => SelectprofilepicPage()
           },
-          theme: ThemeData(
-        primarySwatch: Colors.teal
-    ),
     );
   }
 }
