6f4cfce9dd157a9fc0f098372ee43aee034af17d
Bishal das <31515160+vishaldas22@users.noreply.github.com>
2019-01-30 10:30:55 +0530

    Update healthScreen.dart

--- lib/categoryScreens/healthScreen.dart
+++ lib/categoryScreens/healthScreen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:news/detailScreen.dart';
+import 'package:news/loader.dart';
 import 'package:news/model/businessModel.dart';
 
 import 'package:http/http.dart' as http;
@@ -32,12 +33,16 @@ class HealthScreen extends StatefulWidget {
 class _HealthScreenState extends State<HealthScreen> {
   var list_articles;
   var refreshKey = GlobalKey<RefreshIndicatorState>();
+  Future _data;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     refreshListArticle();
+    setState(() {
+      _data = list_articles;
+    });
   }
 
   Future<Null> refreshListArticle() async {
@@ -55,11 +60,15 @@ class _HealthScreenState extends State<HealthScreen> {
     return MaterialApp(
       title: "NEWS",
       theme: ThemeData(primarySwatch: Colors.teal),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text("Health News",style: TextStyle(
             color: Colors.black
           ),),
+          leading: IconButton(icon: Icon(Icons.arrow_back,color: Colors.black,), onPressed: (){
+            Navigator.pop(context);
+          }),
           elevation: 0.0,
           backgroundColor: Colors.white,
           centerTitle: true,
@@ -84,6 +93,9 @@ class _HealthScreenState extends State<HealthScreen> {
                                   content: article.content,
                                   image: article.urlToImage,
                                   author: article.author,
+                                  publishedAt: article.publishedAt,
+                                  url: article.url,
+                                  description: article.description,
                                 ))),
                         child: Card(
                           elevation: 1.0,
@@ -151,7 +163,24 @@ class _HealthScreenState extends State<HealthScreen> {
                           .toList(),
                     );
                   }
-                  return CircularProgressIndicator();
+                  return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          ColorLoader4(
+                            dotOneColor: Colors.pink,
+                            dotTwoColor: Colors.amber,
+                            dotThreeColor: Colors.deepOrange,
+                            dotType: DotType.diamond,
+                            duration: Duration(milliseconds: 1200),
+                          ),
+                          SizedBox(height: 5.0,),
+                          Text(
+                            'Fetching news please wait...',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ));
                 },
               ),
               key: refreshKey,
